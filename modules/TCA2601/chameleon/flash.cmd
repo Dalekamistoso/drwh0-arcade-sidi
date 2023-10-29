26d59d2ffe14326f3d707179adad5ed0fe86fcd7
Dalekamistoso (DrWh0) <67012746+Dalekamistoso@users.noreply.github.com>
2023-10-29 19:24:07 +0100

    Delete modules directory

--- modules/TCA2601/chameleon/flash.cmd
+++
@@ -1,2 +0,0 @@
-C:\data\archiv\chameleon\Chameleon_Beta-8f\ChaCo_Win32\chacocmd.exe --flashrbf 0 C:\Users\Frank\Documents\GitHub\TCA2601\quartus\TCA2601.rbf
-C:\data\archiv\chameleon\Chameleon_Beta-8f\ChaCo_Win32\chacocmd.exe --start 0
