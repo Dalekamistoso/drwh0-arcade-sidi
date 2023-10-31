a33ea0d10a023420235ebb2f588e93d6e5158cb4
Dalekamistoso (DrWh0) <67012746+Dalekamistoso@users.noreply.github.com>
2023-10-31 18:14:52 +0100

    Add files via upload

---
+++ Namco Hardware/Namco Pacman Hardware/Pengo/rtl/Pengo.sv
@@ -0,0 +1,185 @@
+//============================================================================
+//  Arcade: Pengo
+//
+//  Port to MiSTer
+//  Copyright (C) 2017 Sorgelig
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+
+module Pengo(
+	output        LED,						
+	output  [5:0] VGA_R,
+	output  [5:0] VGA_G,
+	output  [5:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+	output        AUDIO_L,
+	output        AUDIO_R,	
+	input         SPI_SCK,
+	output        SPI_DO,
+	input         SPI_DI,
+	input         SPI_SS2,
+	input         SPI_SS3,
+	input         CONF_DATA0,
+	input         CLOCK_27
+);
+
+`include "rtl\build_id.v" 
+
+localparam CONF_STR = {
+	"Pengo;;",
+	"O2,Rotate Controls,Off,On;",
+	"O34,Scanlines,Off,25%,50%,75%;",
+	"O5,Blend,Off,On;",
+	"T0,Reset;",
+	"V,v1.20.",`BUILD_DATE
+};
+
+wire        rotate = status[2];
+wire  [1:0] scanlines = status[4:3];
+wire        blend = status[5];
+
+assign LED = 1;
+assign AUDIO_R = AUDIO_L;
+
+wire clk_sys, clk_snd;
+wire pll_locked;
+pll pll(
+	.inclk0(CLOCK_27),
+	.areset(0),
+	.c0(clk_sys),
+	.locked(pll_locked)
+	);
+
+reg ce_6m;
+always @(posedge clk_sys) begin
+	reg [1:0] div;
+	div <= div + 1'd1;
+	ce_6m <= !div;
+end
+
+wire [31:0] status;
+wire  [1:0] buttons;
+wire  [1:0] switches;
+wire  [7:0] joystick_0;
+wire  [7:0] joystick_1;
+wire        scandoublerD;
+wire        ypbpr;
+wire        no_csync;
+wire        key_strobe;
+wire        key_pressed;
+wire  [7:0] key_code;
+wire  [7:0] audio;
+wire        hs, vs;
+wire        hb, vb;
+wire        blankn = ~(hb | vb);
+wire  [2:0] r,g;
+wire  [1:0] b;
+
+PACMAN_MACHINE Pengo(
+	.video_r(r),
+	.video_g(g),
+	.video_b(b),
+	.hsync(hs),
+	.vsync(vs),
+	.h_blank(hb),
+	.v_blank(vb),
+	.audio(audio),
+	.in0_reg(~{m_fireA, 2'b00,m_coin1,m_right,m_left,m_down,m_up}),
+	.in1_reg(~{1'b0, m_two_players, m_one_player, 5'b00000}),
+	.dipsw1_reg(8'b11100000),
+	.dipsw2_reg(8'b11001100),
+	.RESET(status[0] | buttons[1]),
+	.CLK(clk_sys),
+	.ENA_6(ce_6m)
+	);
+
+mist_video #(.COLOR_DEPTH(3),.SD_HCNT_WIDTH(10)) mist_video(
+	.clk_sys(clk_sys),
+	.SPI_SCK(SPI_SCK),
+	.SPI_SS3(SPI_SS3),
+	.SPI_DI(SPI_DI),
+	.R(blankn ? r : 0),
+	.G(blankn ? g : 0),
+	.B(blankn ? {b, 1'b0} : 0),
+	.HSync(~hs),
+	.VSync(~vs),
+	.VGA_R(VGA_R),
+	.VGA_G(VGA_G),
+	.VGA_B(VGA_B),
+	.VGA_VS(VGA_VS),
+	.VGA_HS(VGA_HS),
+	.rotate({1'b1,rotate}),
+	.scandoubler_disable(scandoublerD),
+	.scanlines(scanlines),
+	.ce_divider(1'b1),
+	.blend(blend),
+	.ypbpr(ypbpr)
+	);
+
+user_io #(
+	.STRLEN(($size(CONF_STR)>>3)))
+user_io(
+	.clk_sys        (clk_sys        ),
+	.conf_str       (CONF_STR       ),
+	.SPI_CLK        (SPI_SCK        ),
+	.SPI_SS_IO      (CONF_DATA0     ),
+	.SPI_MISO       (SPI_DO         ),
+	.SPI_MOSI       (SPI_DI         ),
+	.buttons        (buttons        ),
+	.switches       (switches       ),
+	.scandoubler_disable (scandoublerD	  ),
+	.ypbpr          (ypbpr          ),
+	.no_csync       (no_csync       ),
+	.key_strobe     (key_strobe     ),
+	.key_pressed    (key_pressed    ),
+	.key_code       (key_code       ),
+	.joystick_0     (joystick_0     ),
+	.joystick_1     (joystick_1     ),
+	.status         (status         )
+	);
+
+dac #(
+	.C_bits(8))
+dac(
+	.clk_i(clk_sys),
+	.res_n_i(1),
+	.dac_i(audio),
+	.dac_o(AUDIO_L)
+	);
+
+// controls
+wire m_up, m_down, m_left, m_right, m_fireA, m_fireB, m_fireC, m_fireD, m_fireE, m_fireF;
+wire m_up2, m_down2, m_left2, m_right2, m_fire2A, m_fire2B, m_fire2C, m_fire2D, m_fire2E, m_fire2F;
+wire m_tilt, m_coin1, m_coin2, m_coin3, m_coin4, m_one_player, m_two_players, m_three_players, m_four_players;
+
+arcade_inputs inputs (
+	.clk         ( clk_sys     ),
+	.key_strobe  ( key_strobe  ),
+	.key_pressed ( key_pressed ),
+	.key_code    ( key_code    ),
+	.joystick_0  ( joystick_0  ),
+	.joystick_1  ( joystick_1  ),
+	.rotate      ( rotate      ),
+	.orientation ( 2'b11       ),
+	.joyswap     ( 1'b0        ),
+	.oneplayer   ( 1'b1        ),
+	.controls    ( {m_tilt, m_coin4, m_coin3, m_coin2, m_coin1, m_four_players, m_three_players, m_two_players, m_one_player} ),
+	.player1     ( {m_fireF, m_fireE, m_fireD, m_fireC, m_fireB, m_fireA, m_up, m_down, m_left, m_right} ),
+	.player2     ( {m_fire2F, m_fire2E, m_fire2D, m_fire2C, m_fire2B, m_fire2A, m_up2, m_down2, m_left2, m_right2} )
+);
+
+endmodule 
---
+++ Namco Hardware/Namco Pacman Hardware/ReadMe.txt
@@ -0,0 +1,9 @@
+Games that should work on this hardware
+
+Nibbler
+Fantasy
+Pioneer Balloon
+Vanguard
+Zarzon
+Piranha
+Titan
